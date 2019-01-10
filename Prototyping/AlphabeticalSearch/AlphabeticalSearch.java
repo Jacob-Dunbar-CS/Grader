d9f03dcf1d52b4d75158324b21a10c57f75125f7
Jacob-Dunbar-CS <j.dunbar3@share.epsb.ca>
2019-01-10 11:05:59 -0700

    update

--- Prototyping/AlphabeticalSearch/AlphabeticalSearch.java
+++ Prototyping/AlphabeticalSearch/AlphabeticalSearch.java
@@ -2,6 +2,15 @@ public class AlphabeticalSearch
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Yeet"); //Debugging line
+		//System.out.println("\nYeet"); //Debugging line
+		
+		//Input String and Verify
+		String[] word = {"The","quick","brown","fox","jumps","over","the","lazy","dog."};
+		for (int i = 0; i < word.length; i++)
+		{
+			System.out.println(word[i]); //Debugging line
+		}
+		//Compare two words
+		System.out.println(word[0].compareTo(word[6]));
 	}
 }
