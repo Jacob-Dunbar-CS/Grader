b9ff4dc75068741529374646ca1c3ae474b477a7
Jacob-Dunbar-CS <j.dunbar3@share.epsb.ca>
2019-01-14 11:46:02 -0700

    revised

--- Prototyping/AlphabeticalSearch/A/A.java
+++ Prototyping/AlphabeticalSearch/A/A.java
@@ -1,41 +1,41 @@
 public class A
 {
-	void sorting(String lowerCaseArr[])
+	void sorting(String lowerCaseWords[])
 	{
-		int n = lowerCaseArr.length;
+		int n = lowerCaseWords.length;
 		for (int i = 0; i < n-1; i++)
 		{
 			for (int o = 0; o < n-i-1; o++)
 			{
-				if (lowerCaseArr[o].compareTo(lowerCaseArr[o+1]) > 0)
+				if (lowerCaseWords[o].compareTo(lowerCaseWords[o+1]) > 0)
 				{
-					String save = lowerCaseArr[o];
-					lowerCaseArr[o] = lowerCaseArr[o+1];
-					lowerCaseArr[o+1] = save;
+					String save = lowerCaseWords[o];
+					lowerCaseWords[o] = lowerCaseWords[o+1];
+					lowerCaseWords[o+1] = save;
 				}
 			}
 		}
 	}
-	void printArray(String lowerCaseArr[])
+	void printArray(String lowerCaseWords[])
 	{
-		int n = lowerCaseArr.length;
+		int n = lowerCaseWords.length;
 		for(int i = 0; i < n; i++)
 		{
-			System.out.println(lowerCaseArr[i]);
+			System.out.println(lowerCaseWords[i]);
 		}
 		System.out.println();
 	}
 	public static void main (String[] args)
 	{
 		A ob = new A();
-		String[] arr = {"The", "quick", "brown","fox", "jumps","over", "the", "lazy", "dog."};
-	String[] lowerCaseArr = new String[arr.length];
-		for (int i = 0; i < arr.length; i++)
+		String[] words = {"The", "quick", "brown","fox", "jumps","over", "the", "lazy", "dog."};
+		String[] lowerCaseWords = new String[words.length];
+		for (int i = 0; i < words.length; i++)
 		{
-			lowerCaseArr[i] = arr[i].toLowerCase();
+			lowerCaseWords[i] = words[i].toLowerCase();
 		}
-		ob.sorting(lowerCaseArr);
+		ob.sorting(lowerCaseWords);
 		System.out.println("Sorted array");
-		ob.printArray(lowerCaseArr);
+		ob.printArray(lowerCaseWords);
 	}
 }
